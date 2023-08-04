32b09b433152f56418ef04808756d9821b5ec540
Jake <56329025+jakeo-dev@users.noreply.github.com>
2023-08-04 08:29:09 -0700

    ACCIDENTALLY UPLOADED THE WRONG THING - More fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig } */
 
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  darkMode: 'class',
+  content: ['./**/*.{html,js}'],
   theme: {
     fontFamily: {
       'LexendDeca': ['Lexend Deca', 'sans-serif'],
-      'Sen': ['Sen', 'sans-serif'],
-      'NotoSerif': ['Noto Serif', 'sans-serif']
+      'Calistoga': ['Calistoga', 'sans-serif']
     },
     extend: {
-      width: {
-        '1/16': '6.25%',
-        '1/20': '5%',
-      }
+
     }
   },
   plugins: [],
