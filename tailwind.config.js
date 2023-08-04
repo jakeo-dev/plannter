d01d1f23ea88258f2b5fe2022ba48f715cd9d626
Jake <56329025+jakeo-dev@users.noreply.github.com>
2023-08-04 08:28:04 -0700

    More fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-/** @type {import('@types/tailwindcss/tailwind-config').TailwindConfig } */
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 
 module.exports = {
-  darkMode: 'class',
-  content: ['./**/*.{html,js}'],
+  content: ["./**/*.{html,js}"],
   theme: {
     fontFamily: {
       'LexendDeca': ['Lexend Deca', 'sans-serif'],
-      'Calistoga': ['Calistoga', 'sans-serif']
+      'Sen': ['Sen', 'sans-serif'],
+      'NotoSerif': ['Noto Serif', 'sans-serif']
     },
     extend: {
-
+      width: {
+        '1/16': '6.25%',
+        '1/20': '5%',
+      }
     }
   },
   plugins: [],
